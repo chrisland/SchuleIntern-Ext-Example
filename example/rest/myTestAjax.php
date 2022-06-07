b1893b004afed2b2c7c740cd056306b7f1436389
Christian Marienfeld <post@zwiebelgasse.de>
2022-06-07 04:27:59 +0200

    prepare to version 7

--- example/rest/myTestAjax.php
+++ example/rest/myTestAjax.php
@@ -26,21 +26,33 @@ class myTestAjax extends AbstractRest {
 		return 'GET';
 	}
 
-	/**
-	 * Ist eine System Authentifizierung nötig? (mit API key)
-	 * @return Boolean
-	 */
-	public function needsSystemAuth() {
-		return false;
-	}
 
-	/**
-	 * Muss der Benutzer eingeloggt sein?
-	 * @return Boolean
-	 */
-	public function needsUserAuth() {
-		return true;
-	}
+    /**
+     * Muss der Benutzer eingeloggt sein?
+     * Ist Eine Session vorhanden
+     * @return Boolean
+     */
+    public function needsUserAuth() {
+        return true;
+    }
+
+    /**
+     * Ist eine Admin berechtigung nötig?
+     * only if : needsUserAuth = true
+     * @return Boolean
+     */
+    public function needsAdminAuth()
+    {
+        return false;
+    }
+    /**
+     * Ist eine System Authentifizierung nötig? (mit API key)
+     * only if : needsUserAuth = false
+     * @return Boolean
+     */
+    public function needsSystemAuth() {
+        return false;
+    }
 
 }	
 
